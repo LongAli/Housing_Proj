9951e2405cbac2aa80afc02abc8d4cd1c7be91b7
Ali L <alil@macbook-pro.lan>
2021-10-15 20:17:48 -0700

    refactor: update footer to the newest design

--- FrontEnd/src/components/NavBar/NavBar.tsx
+++ FrontEnd/src/components/NavBar/NavBar.tsx
@@ -38,7 +38,7 @@ const NavBar: FunctionComponent = () => {
       <div className={styles.container}>
         <div className="mr-auto">
           <a href="/housing">
-            <landingIcons.logo className={styles.logo} />
+            <landingIcons.logo className={styles.giLogo} />
           </a>
         </div>
         <Row className="align-items-center">
@@ -58,19 +58,19 @@ const NavBar: FunctionComponent = () => {
               />
             </>
           ) : (
-            <>
-              <Link href="/about" undecorated>
-                <h5 className="mb-0">About</h5>
-              </Link>
-              <Button
-                size="secondary"
-                variant="solid"
-                onClick={() => dispatch(showLogin())}
-              >
-                Get Started
+              <>
+                <Link href="/about" undecorated>
+                  <h5 className="mb-0">About</h5>
+                </Link>
+                <Button
+                  size="secondary"
+                  variant="solid"
+                  onClick={() => dispatch(showLogin())}
+                >
+                  Get Started
               </Button>
-            </>
-          )}
+              </>
+            )}
         </Row>
       </div>
     </Navbar>
